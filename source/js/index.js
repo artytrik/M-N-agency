cbb6f67bcdd05c45980dfbf1347f6402f174166c
artytrik <tema-luch@mail.ru>
2019-12-24 15:48:02 +0300

    Добавляет якоря на ссылки

--- source/js/index.js
+++ source/js/index.js
@@ -88,3 +88,23 @@ $("#form").submit(function (e) { // Устанавливаем событие о
     onSendSuccess();
    });
 });
+
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+
+    $('html, body').animate({scrollTop: dn}, 1000);
+
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+});
