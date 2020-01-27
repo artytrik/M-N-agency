8f535b4fcad05f42ce3e4734df86d687a92ba706
artytrik <tema-luch@mail.ru>
2020-01-27 17:41:41 +0300

    Дополнительно меняет mail

--- build/send.php
+++ build/send.php
@@ -13,7 +13,7 @@
     $data['errors'] = $errors;
   } else {
     $to = "mail@m-n.agency";; // Здесь нужно написать e-mail, куда будут приходить письма
-    $from = "site@m-n.agency"; // this is the sender's Email address
+    $from = "mail@m-n.agency"; // this is the sender's Email address
 
     $first_name = $_POST['name'];
     $subject = "Форма отправки сообщений с сайта M&N Agency";
